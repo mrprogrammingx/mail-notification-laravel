e0e33bc199596ec6fbad68c99cba810c4e1ba095
amir <amirjoun2@gmail.com>
2025-03-09 21:23:19 +0400

    Added sendMail and sendNotification routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MeetupController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,3 +16,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+Route::get('/sendMail',[MeetupController::class, 'sendMail']);
+Route::get('/sendNotification',[MeetupController::class, 'sendNotification']);
