3573b9774a4fea84db559aebce278e51296664dd
amir <amirjoun2@gmail.com>
2025-03-09 21:33:28 +0400

    Added for sending Notification after login

--- app/Notifications/LoggedIn.php
+++ app/Notifications/LoggedIn.php
@@ -2,19 +2,21 @@
 
 namespace App\Notifications;
 
+use Illuminate\Http\Request;
 use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Stevebauman\Location\Facades\Location;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
 
-class NewSignUp extends Notification
+class LoggedIn extends Notification
 {
     use Queueable;
 
     /**
      * Create a new notification instance.
      */
-    public function __construct(public string $name, public string $email)
+    public function __construct(public Request $request)
     {
         //
     }
@@ -34,10 +36,15 @@ class NewSignUp extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
+        $country = ($position = Location::get()) ? $position->countryName : "UNKNOWN";
+
         return (new MailMessage)
-            ->subject('New Meetup Registration')
-            ->line('You have a new registered user for your meetup!')
-            ->line("{$this->name} ({$this->email}) has registered for the meetup.")
+            ->line('Dear ' . ucfirst($notifiable->name))
+            ->line('There is a new logged in on your account.')
+            ->line('Time of logged in: '. now())
+            ->line('IP Address: ' . $this->request->ip())
+            ->line('Device: ' . $this->request->header('User-Agent'))
+            ->line('Location: ' . $country)
             ->line('Thank you for using our application!');
     }
 
