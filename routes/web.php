2b75b9ec8609ca1f18e169ad790fa3ffdc8a8ddf
amir <amirjoun2@gmail.com>
2025-03-10 08:18:46 +0400

    Added AuthMiddleware to routes

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    
+    Route::get('/sendMail',[MeetupController::class, 'sendMail']);
+    Route::get('/sendNotification',[MeetupController::class, 'sendNotification']);
 });
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
-
-Route::get('/sendMail',[MeetupController::class, 'sendMail']);
-Route::get('/sendNotification',[MeetupController::class, 'sendNotification']);
